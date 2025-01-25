a20ce16cc09dad20ddefbb534f64496d1d0e755b
Cristian <cristian.guamba>
2025-01-25 18:11:58 -0500

    Cambio con selenium para manejar navegador.

--- imdb_web_scraping.py
+++ imdb_web_scraping.py
@@ -1,29 +1,101 @@
-from urllib.request import urlopen as uReq
-from bs4 import BeautifulSoup as soup
-import pandas as pdp
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
-my_url = "http://www.imdb.com/search/title?sort=num_votes,desc&start=1&title_type=feature&year=1950,2025"
+# Configura Chrome para trabajar en modo headless (sin ventana)
+options = Options()
 
-uClient = uReq(my_url)
-page_html = uClient.read()
-uClient.close()
+# Ruta al ejecutable de ChromeDriver
+service = Service(
+    "D:/CRIS/Documentos/Israel/semestre_9/Tendencias_innovadoras_de_la_profesion/Proyecto-web-scraping/Proyecto-web-scraping/chromedriver-win64/chromedriver.exe")
 
-page_soup = soup(page_html, "html.parser")
+my_url = "https://www.imdb.com/search/title/?title_type=feature&release_date=1950-01-01,2025-12-31&sort=num_votes,desc"
 
-filename = "imdb_m.csv"
-f = open(filename, "w")
+# Crea el navegador
+driver = webdriver.Chrome(service=service, options=options)
+driver.get(my_url)
 
-headers = "Name, Year, Runtime \n"
+# Esperar explícitamente a que un elemento clave esté presente
+element = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "ipc-metadata-list-summary-item"))
+)
+# total_paginas = 10426
+total_paginas = 10
+
+# # # # Descomentar para hacer clic en el páginador y cargar 50 peliculas nuevas en cada iteración.
+#
+# for i in range(total_paginas):
+#     btn_paginator = WebDriverWait(driver, 10).until(
+#         EC.element_to_be_clickable(
+#             (By.CLASS_NAME,
+#              "ipc-btn.ipc-btn--single-padding.ipc-btn--center-align-content.ipc-btn--default-height.ipc-btn--core-base.ipc-btn--theme-base.ipc-btn--button-radius.ipc-btn--on-accent2.ipc-text-button.ipc-see-more__button")
+#         )
+#     )
+#     # btn_paginator = driver.find_element(By.CLASS_NAME, "ipc-btn.ipc-btn--single-padding.ipc-btn--center-align-content.ipc-btn--default-height.ipc-btn--core-base.ipc-btn--theme-base.ipc-btn--button-radius.ipc-btn--on-accent2.ipc-text-button.ipc-see-more__button")
+#     ActionChains(driver).move_to_element(btn_paginator).perform()
+#     btn_paginator.click()
+
+s_container = driver.find_elements(By.CLASS_NAME, "sc-2bfd043a-3.jpWwpQ.dli-parent")
+
+# Archivo de salida
+filename = "./imdb_m.csv"
+f = open(filename, "w", encoding="utf-8")
+
+# Encabezados del archivo CSV
+headers = "Pos,Title,Year,Runtime,Clasif,Rating,VoteCount,Score,Plot\n"
 f.write(headers)
 
-for container in containers:
-    name = container.img["alt"]
-    year_mov = container.findAll("span", {"class": "lister-item-year"})
-    year = year_mov[0].text
-    runtime_mov = container.findAll("span", {"class": "runtime"})
-    runtime = runtime_mov[0].text
+separador = ";"
+salto = "\n"
+count = 0
 
-    print(name + "," + year + "," + runtime + "\n")
-    f.write(name + "," + year + "," + runtime + "\n")
+for container in s_container:
+    # Título de la película
+    name = container.find_element(By.CLASS_NAME, "ipc-title__text").text
 
+    # Dividir el texto en número y título
+    parts = name.split(".", 1)  # Dividimos solo en el primer punto
+    number = parts[0].strip()  # Número, eliminando espacios
+    title = parts[1].strip()  # Título, eliminando espacios
+
+    f.write(number + separador)
+    f.write(title + separador)
+
+    year_runtime_clasif_mov = container.find_elements(By.CLASS_NAME, "sc-300a8231-7")
+
+    year_mov, runtime_mov, clasif_mov = [element.text if element else "N/A" for element in year_runtime_clasif_mov]
+
+    f.write(year_mov + separador)
+    f.write(runtime_mov + separador)
+    f.write(clasif_mov + separador)
+
+    rating_group = container.find_element(By.CLASS_NAME, "ipc-rating-star--rating")
+    rating = rating_group.text if rating_group else "N/A"
+    f.write(rating + separador)
+
+    voteCount_group = container.find_element(By.CLASS_NAME, "ipc-rating-star--voteCount")
+    voteCount = voteCount_group.text if voteCount_group else "N/A"
+    voteCount = voteCount.lstrip().replace('(', '').replace(')', '')
+    f.write(voteCount + separador)
+
+    score_group = container.find_element(By.CLASS_NAME, "sc-b0901df4-0")
+    score = score_group.text if score_group else "N/A"
+    f.write(score + separador)
+
+    plot_group = container.find_element(By.CLASS_NAME, "ipc-html-content-inner-div")
+    plot = plot_group.text if plot_group else "N/A"
+    plot = plot.lstrip().rstrip().replace(';', '')
+    f.write(plot + separador)
+
+    f.write(salto)
+
+# Cierra el archivo
 f.close()
+
+# Cierra el navegador
+driver.quit()
